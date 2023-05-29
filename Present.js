114a3fa0dbdf689b380ef7721ce8a1fdd86721ed
RutgerPijnenburg <132466058+RutgerPijnenburg@users.noreply.github.com>
2023-05-29 15:50:40 +0200

    Add files via upload

---
+++ Present.js
@@ -0,0 +1,182 @@
+
+
+  function AppearPIV() {
+    document.getElementById('PIV').style.opacity = "1";
+    document.getElementById('PIV').style.height = "100%";
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  
+  function DisappearPIV() {
+    document.getElementById('PIV').style.opacity = "0";
+    document.getElementById('PIV').style.height = "0";
+  }
+
+  function AppearMDC() {
+    document.getElementById('MDC').style.opacity = "1";
+    document.getElementById('MDC').style.height = "100%";
+    DisappearPIV();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearMDC() {
+    document.getElementById('MDC').style.opacity = "0";
+    document.getElementById('MDC').style.height = "0";
+  }
+
+  function AppearCA() {
+    document.getElementById('CA').style.opacity = "1";
+    document.getElementById('CA').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearCA() {
+    document.getElementById('CA').style.opacity = "0";
+    document.getElementById('CA').style.height = "0";
+  }
+
+  function AppearBE() {
+    document.getElementById('BE').style.opacity = "1";
+    document.getElementById('BE').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearBE() {
+    document.getElementById('BE').style.opacity = "0";
+    document.getElementById('BE').style.height = "0";
+  }
+
+  function AppearUS() {
+    document.getElementById('US').style.opacity = "1";
+    document.getElementById('US').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearUS() {
+    document.getElementById('US').style.opacity = "0";
+    document.getElementById('US').style.height = "0";
+  }
+  
+  function AppearDR() {
+    document.getElementById('DR').style.opacity = "1";
+    document.getElementById('DR').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearDR() {
+    document.getElementById('DR').style.opacity = "0";
+    document.getElementById('DR').style.height = "0";
+  }
+  
+  function AppearTR() {
+    document.getElementById('TR').style.opacity = "1";
+    document.getElementById('TR').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearPro();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearTR() {
+    document.getElementById('TR').style.opacity = "0";
+    document.getElementById('TR').style.height = "0";
+  }
+  
+  function AppearPro() {
+    document.getElementById('Pro').style.opacity = "1";
+    document.getElementById('Pro').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearGoals();
+    DisappearExternal();
+  }
+  function DisappearPro() {
+    document.getElementById('Pro').style.opacity = "0";
+    document.getElementById('Pro').style.height = "0";
+  }
+  
+  function AppearGoals() {
+    document.getElementById('Goals').style.opacity = "1";
+    document.getElementById('Goals').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearExternal();
+  }
+  function DisappearGoals() {
+    document.getElementById('Goals').style.opacity = "0";
+    document.getElementById('Goals').style.height = "0";
+  }
+  
+  function AppearExternal() {
+    document.getElementById('External').style.opacity = "1";
+    document.getElementById('External').style.height = "100%";
+    DisappearPIV();
+    DisappearMDC();
+    DisappearCA();
+    DisappearBE();
+    DisappearUS();
+    DisappearDR();
+    DisappearTR();
+    DisappearPro();
+    DisappearGoals();
+  }
+  function DisappearExternal() {
+    document.getElementById('External').style.opacity = "0";
+    document.getElementById('External').style.height = "0";
+  }
